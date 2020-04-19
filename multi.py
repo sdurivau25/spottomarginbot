0daa98d6577d41effd1084c72e7c71cea9f9f302
sdurivau25 <63911235+sdurivau25@users.noreply.github.com>
2020-04-19 19:38:42 +0200

    Add files via upload

--- multi.py
+++ multi.py
@@ -52,35 +52,45 @@ class Bot(Thread):
         self.usdt_quantity = round(float((self.buy_pourcentage*self.margin_usdt)),5)
         #quantity to be bought with margin_asset
         self.asset_quantity = round(float((self.buy_pourcentage*self.margin_usdt)/(self.last_order['price']) - 0.00001),5)
-        #place order
-        self.client.create_margin_order(
-                symbol=self.margin_monnaie,
-                side=SIDE_BUY,
-                type=ORDER_TYPE_MARKET,
-                quantity = self.asset_quantity,
-                )
-        #actualise spot_wallet
-        self.spot_usdt=self.spot_usdt - self.lastorder['executedQty']*self.last_order['price']
-        self.spot_asset=self.spot_asset + self.lastorder['executedQty']
-        if round(float(self.usdt),5) - 0.00001 > 0 :
-            self.spot_usdt=round(float(self.usdt),5) - 0.00001
+        #Check les quantites
+        if self.asset_quantity*float(self.last_order['price']) > 11 :
+            #place order
+            self.client.create_margin_order(
+                    symbol=self.margin_monnaie,
+                    side=SIDE_BUY,
+                    type=ORDER_TYPE_MARKET,
+                    quantity = self.asset_quantity,
+                    )
+            #prix d achat
+            self.buy_price = client.get_all_margin_orders(symbol=margin_monnaie)[-1]['price']
+            #actualise spot_wallet
+            self.spot_usdt=self.spot_usdt - self.lastorder['executedQty']*self.last_order['price']
+            self.spot_asset=self.spot_asset + self.lastorder['executedQty']
+            if round(float(self.usdt),5) - 0.00001 > 0 :
+                self.spot_usdt=round(float(self.usdt),5) - 0.00001
+            else :
+                self.spot_usdt=0.0
+            if round(float(self.asset),5)- 0.00001 > 0
+                self.spot_asset=round(float(self.asset),5)- 0.00001
+            else :
+                self.spot_asset=0.0
+            #actualise margin_wallet
+            self.margin_usdt=self.margin_usdt - self.usdt_quantity 
+            self.margin_asset=self.margin_asset + self.asset_quantity
+            if round(float(self.margin_usdt),5) - 0.00001 > 0 :
+                self.margin_usdt=round(float(self.margin_usdt),5) - 0.00001
+            else : 
+                self.margin_usdt=0.0
+            if round(float(self.margin_asset),5)- 0.00001 > 0 :
+                self.margin_asset=round(float(self.margin_asset),5)- 0.00001
+            else :
+                self.margin_asset=0.0
+            #log
+            self.log('Achat de {} {} au prix de {}'.format(self.asset_quantity,self.margin_monnaie,self.buy_price))
+
         else :
-            self.spot_usdt=0.0
-        if round(float(self.asset),5)- 0.00001 > 0
-            self.spot_asset=round(float(self.asset),5)- 0.00001
-        else :
-            self.spot_asset=0.0
-        #actualise margin_wallet
-        self.margin_usdt=self.margin_usdt - self.usdt_quantity 
-        self.margin_asset=self.margin_asset + self.asset_quantity
-        if round(float(self.margin_usdt),5) - 0.00001 > 0 :
-            self.margin_usdt=round(float(self.margin_usdt),5) - 0.00001
-        else : 
-            self.margin_usdt=0.0
-        if round(float(self.margin_asset),5)- 0.00001 > 0 :
-            self.margin_asset=round(float(self.margin_asset),5)- 0.00001
-        else :
-            self.margin_asset=0.0
+            self.log('ERROR 2 : Order too low, buy skipped')
+
         
         
     def sell_as_strategy(self):
@@ -89,37 +99,43 @@ class Bot(Thread):
         self.sell_pourcentage = (sell_spot_used_asset)/(self.spot_asset)
         #asset used by margin to sell
         self.asset_quantity = round(float(self.sell_pourcentage*self.margin_asset),5)
-        #place order
-        client.create_margin_order(
-                        symbol=self.margin_monnaie,
-                        side=SIDE_SELL,
-                        type=ORDER_TYPE_MARKET,
-                        quantity=self.asset_quantity,
-                        )
-        #prix de vente
-        sell_price = client.get_all_margin_orders(symbol=margin_monnaie)[-1]['price']
-        #actualise spot_wallet
-        self.spot_usdt=self.spot_usdt + self.lastorder['executedQty']*self.last_order['price']
-        self.spot_asset=self.spot_asset - self.lastorder['executedQty']
-        if round(float(self.spot_usdt),5) - 0.00001 > 0 :
-            self.spot_usdt=round(float(self.spot_usdt),5) - 0.00001
-        else : 
-            self.spot_usdt=0.0
-        if round(float(self.spot_asset),5) - 0.00001 > 0 :
-            self.spot_asset=round(float(self.spot_asset),5) - 0.00001
+        #check les quantites
+        if self.asset_quantity*float(self.last_order['price']) > 11 :
+            #place order
+            client.create_margin_order(
+                            symbol=self.margin_monnaie,
+                            side=SIDE_SELL,
+                            type=ORDER_TYPE_MARKET,
+                            quantity=self.asset_quantity,
+                            )
+            #prix de vente
+            self.sell_price = client.get_all_margin_orders(symbol=margin_monnaie)[-1]['price']
+            #actualise spot_wallet
+            self.spot_usdt=self.spot_usdt + self.lastorder['executedQty']*self.last_order['price']
+            self.spot_asset=self.spot_asset - self.lastorder['executedQty']
+            if round(float(self.spot_usdt),5) - 0.00001 > 0 :
+                self.spot_usdt=round(float(self.spot_usdt),5) - 0.00001
+            else : 
+                self.spot_usdt=0.0
+            if round(float(self.spot_asset),5) - 0.00001 > 0 :
+                self.spot_asset=round(float(self.spot_asset),5) - 0.00001
+            else :
+                self.spot_asset=0.0
+            #actualise margin_wallet
+            self.margin_usdt=self.margin_usdt + self.asset_quantity*sell_price
+            self.margin_asset=self.margin_asset - self.asset_quantity
+            if round(float(self.margin_usdt),5) - 0.00001 > 0 :
+                self.margin_usdt=round(float(self.margin_usdt),5) - 0.00001
+            else :
+                self.margin_usdt=0.0
+            if round(float(self.margin_asset),5)- 0.00001 > 0 :
+                self.margin_asset=round(float(self.margin_asset),5)- 0.00001
+            else :
+                self.margin_asset=0.0
+            #log
+            self.log('Vente de {} {} au prix de {}'.format(self.asset_quantity,self.margin_monnaie,self.buy_price))
         else :
-            self.spot_asset=0.0
-        #actualise margin_wallet
-        self.margin_usdt=self.margin_usdt + self.asset_quantity*sell_price
-        self.margin_asset=self.margin_asset - self.asset_quantity
-        if round(float(self.margin_usdt),5) - 0.00001 > 0 :
-            self.margin_usdt=round(float(self.margin_usdt),5) - 0.00001
-        else :
-            self.margin_usdt=0.0
-        if round(float(self.margin_asset),5)- 0.00001 > 0 :
-            self.margin_asset=round(float(self.margin_asset),5)- 0.00001
-        else :
-            self.margin_asset=0.0
+            self.log('ERROR 2 : Order too low, sell skipped')
         
 
 
@@ -132,16 +148,18 @@ class Bot(Thread):
                 
                 if self.last_order['side'] == 'BUY':
                     self.log('Nouvel achat repere, copie en cours...')
-                    self.buy_all()
+                    self.buy_as_strategy()
+                    self.log('Current status wallet : {} Asset et {} USD'.format(self.margin_asset,self.margin_usdt)
                 elif self.last_order['side'] == 'SELL':
                     self.log('Nouvelle vente reperee, copie en cours...')
-                    self.sell_all()
+                    self.sell_as_strategy()
+                    self.log('Current status wallet : {} Asset et {} USD'.format(self.margin_asset,self.margin_usdt)
                 else:
                     self.log('ERREUR 1: Abort')
                     self.continuer = False
 
                 self.log("En attente du passage du dernier ordre detecte...")
-                while self.client.get_open_orders(symbol=self.monnaie) !=[]:
+                while self.client.get_open_orders(symbol=self.spot_monnaie) !=[]:
                     pass
                 self.log('Bot en ecoute.')
 
