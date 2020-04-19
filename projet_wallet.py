41438547d04cd6838a1063efa795a61b8deddb62
sdurivau25 <63911235+sdurivau25@users.noreply.github.com>
2020-04-19 13:31:53 +0200

    Add files via upload

---
+++ projet_wallet.py
@@ -0,0 +1,9 @@
+from binance.client import Client
+from binance.enums import *
+from time import sleep
+
+wallet_usdt_ethusdt = float(input('usdt dans la strategie ethusdt ?'))
+wallet_eth_ethusdt = 0.0
+wallet_usdt_btcusdt = float(input('usdt dans la strategie btcusdt ?'))
+wallet_btc_btcusdt = 0.0
+
