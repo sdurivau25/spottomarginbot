e8992589eb3c01e19f06335d71b8a556b3ab24ab
sdurivau25 <63911235+sdurivau25@users.noreply.github.com>
2020-04-19 13:33:29 +0200

    Update projet_wallet.py

--- projet_wallet.py
+++ projet_wallet.py
@@ -1,9 +1,20 @@
+#coding:utf-8
+from threading import Thread, RLock
 from binance.client import Client
-from binance.enums import *
-from time import sleep
+from binance.enums import SIDE_BUY, SIDE_SELL, ORDER_TYPE_MARKET
+from time import strftime
 
 wallet_usdt_ethusdt = float(input('usdt dans la strategie ethusdt ?'))
 wallet_eth_ethusdt = 0.0
 wallet_usdt_btcusdt = float(input('usdt dans la strategie btcusdt ?'))
 wallet_btc_btcusdt = 0.0
 
+verrou = Rlock()
+
+def log_func(msg):
+    print(msg)
+
+class Interpreteur(Thread):
+    
+    def __init__(self):
+        print('Initialisation...')
